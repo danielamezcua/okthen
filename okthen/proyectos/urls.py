12a02584576df75ef30374ca94637ae3d564e7bc
daniel amezcua <dany_amezcua@live.com.mx>
2018-03-12 16:10:01 -0600

    Ver proyecto

--- okthen/proyectos/urls.py
+++ okthen/proyectos/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 app_name = 'proyectos'
 
 urlpatterns = [
-    path('ver_proyecto', views.ver_proyecto, name='ver_proyecto'),
+    path('', views.index, name='index'),
+    path('detalle_proyecto/<int:id_proyecto>', views.detalle_proyecto, name='detalle_proyecto'),
     path('agregar_proyecto', views.agregar_proyecto, name='agregar_proyecto'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- okthen/proyectos/views.py
+++ okthen/proyectos/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from workitems.forms import WorkItemForm
 from .models import Proyecto
 from okthen.sessions import validate
 
 
-def ver_proyecto(request):
+def detalle_proyecto(request, id_proyecto):
     valid = validate(request)
     if valid == True:
+        proyecto = get_object_or_404(Proyecto, pk=id_proyecto)
         form_work_item = WorkItemForm()
-        return render(request, 'detalle_proyecto.html', {'form_work_item':form_work_item})
+        return render(request, 'detalle_proyecto.html', {'form_work_item':form_work_item, 'proyecto':proyecto})
+    return valid
+
+def index(request):
+    valid = validate(request)
+    if valid == True:
+        proyectos = Proyecto.objects.all()
+        return render(request, 'index_proyectos.html', {'proyectos':proyectos})
     return valid
 
 def agregar_proyecto(request):
