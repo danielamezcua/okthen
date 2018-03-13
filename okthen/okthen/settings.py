6246740d64658b806afc49155da9e164ec4c476d
daniel amezcua <dany_amezcua@live.com.mx>
2018-03-13 01:32:01 -0600

    hey

--- okthen/okthen/settings.py
+++ okthen/okthen/settings.py
@@ -147,7 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 WSGI_APPLICATION = 'okthen.wsgi.application'
 
