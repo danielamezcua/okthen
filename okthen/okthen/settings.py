8cb2a5ba793c480a17d347ee2f432ef03a65e7a4
daniel amezcua <dany_amezcua@live.com.mx>
2018-03-13 00:59:07 -0600

    settings

--- okthen/okthen/settings.py
+++ okthen/okthen/settings.py
@@ -149,6 +149,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+WSGI_APPLICATION = 'okthen.wsgi.application'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
