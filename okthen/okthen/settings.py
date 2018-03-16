192468741b37ea32d1f7889122f49f4a34234644
daniel amezcua <dany_amezcua@live.com.mx>
2018-03-15 20:04:09 -0600

    heroku

--- okthen/okthen/settings.py
+++ okthen/okthen/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
-
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
